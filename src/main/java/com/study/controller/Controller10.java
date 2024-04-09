551107e8c67964a63104c6a36866fa38248d609f
kooreung <crisishyun@gmail.com>
2024-04-09 10:48:17 +0900

    240409_Model.복합성ArrayList,requestScope

--- src/main/java/com/study/controller/Controller10.java
+++ src/main/java/com/study/controller/Controller10.java
@@ -76,7 +76,16 @@ public class Controller10 {
         model.addAttribute("number1",1);
         model.addAttribute("two",2);
         model.addAttribute("three","3");
+        model.addAttribute("3",30);
+        model.addAttribute("삼",3);
         model.addAttribute("data1","son");
         model.addAttribute("data2","lee");
+    }
+
+    @RequestMapping("sub5")
+    public void method5(Model model) {
+        model.addAttribute("data","값 1");
+        model.addAttribute("데이터","값 2");
+        model.addAttribute("3","삼");
     }
 }
