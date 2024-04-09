71864c9f6a077e382c56a8f2438a1a1116b28028
kooreung <crisishyun@gmail.com>
2024-04-09 09:16:46 +0900

    240409_Controller10

---
+++ src/main/java/com/study/controller/Controller10.java
@@ -0,0 +1,10 @@
+package com.study.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("main10")
+public class Controller10 {
+}
