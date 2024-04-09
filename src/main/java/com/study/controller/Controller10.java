f6669bdc8083e9f7dc8d15ed754d3a4e51750f91
kooreung <crisishyun@gmail.com>
2024-04-09 09:27:54 +0900

    240409_Model.HashMap.List

--- src/main/java/com/study/controller/Controller10.java
+++ src/main/java/com/study/controller/Controller10.java
@@ -2,9 +2,20 @@ package com.study.controller;
 
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.HashMap;
+import java.util.List;
 
 @Controller
 @RequestMapping("main10")
 public class Controller10 {
+    @RequestMapping("sub1")
+    public void method1(Model model) {
+        var data = new HashMap<String, List>();
+        data.put("key1", List.of("java", "css"));
+        data.put("key2", List.of("react", "vue"));
+        model.addAttribute("myList", data);
+    }
 }
---
+++ src/main/webapp/WEB-INF/view/main10/sub1.jsp
@@ -0,0 +1,21 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: admin
+  Date: 2024-04-09
+  Time: 오전 9:22
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+    <p>${myList.key1[1]}</p>
+    <p>${myList.key2[0]}</p>
+    <p>${myList.key2[1]}</p>
+<%-- myList = data 에 접근 --%>
+<%-- key1 = HashMap 에 접근 --%>
+<%-- [1] = HashMap 내 리스트에 접근 --%>
+</body>
+</html>
