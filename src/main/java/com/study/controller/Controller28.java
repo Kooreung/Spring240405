c43d15aee42b6e927cf8c7618addf6a947e77809
kooreung <crisishyun@gmail.com>
2024-04-19 15:30:38 +0900

    240419_JDBC_insert

---
+++ src/main/java/com/study/controller/Controller28.java
@@ -0,0 +1,53 @@
+package com.study.controller;
+
+import com.study.domain.MyBean255CustomerList;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+
+@Controller
+@RequestMapping("main28")
+public class Controller28 {
+    @Autowired
+    private DataSource dataSource;
+
+    @GetMapping("sub1")
+    private void sub1(){
+
+    }
+    @PostMapping("sub1")
+    private String sub2(MyBean255CustomerList customer, RedirectAttributes rttr) throws Exception {
+        String sql = """
+                INSERT INTO Customers
+                (CustomerName, ContactName, Address, City, PostalCode, Country)
+                VALUES (?, ?, ?, ?, ?, ?)
+                """;
+
+        Connection conn = dataSource.getConnection();
+        PreparedStatement pstmt = conn.prepareStatement(sql);
+
+        try (pstmt; conn) {
+            pstmt.setString(1, customer.getName());
+            pstmt.setString(2, customer.getContactName());
+            pstmt.setString(3, customer.getAddress());
+            pstmt.setString(4, customer.getCity());
+            pstmt.setString(5, customer.getPostalCode());
+            pstmt.setString(6, customer.getCountry());
+
+            int rowCount = pstmt.executeUpdate();
+            if (rowCount > 0) {
+                rttr.addFlashAttribute("message", "새 고객이 등록되었습니다.");
+            }
+        }
+
+        return "redirect:/main28/sub1";
+    }
+}
---
+++ src/main/webapp/WEB-INF/view/main28/sub1.jsp
@@ -0,0 +1,48 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: admin
+  Date: 2024-04-19
+  Time: 오후 2:30
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@ taglib prefix="c" uri="jakarta.tags.core" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+
+<body>
+<div>
+    <c:if test="${not empty message}">
+        <div style="padding: 20px; background-color: skyblue">
+                ${message}
+        </div>
+    </c:if>
+</div>
+<h3>고객 정보 입력</h3>
+<form action="" method="post">
+    <div>이름
+        <input type="text" name="name">
+    </div>
+    <div>계약명
+        <input type="text" name="contactName">
+    </div>
+    <div>주소
+        <input type="text" name="address">
+    </div>
+    <div>도시
+        <input type="text" name="city">
+    </div>
+    <div>우편번호
+        <input type="text" name="postalCode">
+    </div>
+    <div>국가
+        <input type="text" name="country">
+    </div>
+    <div>
+        <input type="submit" value="등록">
+    </div>
+</form>
+</body>
+</html>
