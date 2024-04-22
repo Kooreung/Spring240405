f56972e2f13062bf8a258812e8e298582e59fdef
kooreung <crisishyun@gmail.com>
2024-04-22 12:14:22 +0900

    240422_JDBC_update

---
+++ sql/15update.sql
@@ -0,0 +1,28 @@
+USE w3schools;
+
+#UPDATE 테이블명
+#SET 컬럼명 = 바꿀값, 컬럼명 = 바꿀값 ...
+#WHERE 레코드의 조건
+
+#주의 : UPDATE 전에 같은 WHERE 로 꼭 SELECT 시도
+
+SELECT *
+FROM Customers
+WHERE CustomerID = 10;
+
+UPDATE Customers
+SET ContactName = 'GOLD MAN'
+WHERE CustomerID = 10;
+
+UPDATE Customers
+SET Address = 'STARK TOWER',
+    City = 'NEWYORK',
+    PostalCode = '12345'
+WHERE CustomerID = 10;
+
+UPDATE Customers
+SET Country = '영국'
+WHERE Country = 'UK';
+
+UPDATE Customers
+SET Country = '영국';
