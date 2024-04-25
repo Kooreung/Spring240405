8f64cd98f4596f190f57bb69948448c81a5805e4
kooreung <crisishyun@gmail.com>
2024-04-25 10:46:37 +0900

    240425_constraints_Unique

--- sql/21unique.sql
+++ sql/21unique.sql
@@ -18,3 +18,17 @@ INSERT INTO my_table15
 (col1, col2)
 VALUES ('qwe' , 'qwe');
 
+CREATE TABLE my_table16
+(col1 VARCHAR(5),
+col2 VARCHAR(5) UNIQUE,
+col3 VARCHAR(5) NOT NULL UNIQUE);
+
+SELECT *
+FROM my_table16;
+
+DESC my_table16;
+
+INSERT INTO my_table16
+(col1, col2, col3)
+VALUES ( NULL, NULL, '789');
+
