10cfe50e774ffa5c54ae592393cf6893607f61e3
kooreung <crisishyun@gmail.com>
2024-04-25 12:21:57 +0900

    240425_TableManagement_Alter

--- sql/24alter.sql
+++ sql/24alter.sql
@@ -24,6 +24,66 @@ ADD COLUMN address VARCHAR(10);
 SELECT *
 FROM my_table23;
 
-
 ALTER TABLE my_table23
     ADD COLUMN birth DATE AFTER age;
+
+# Column 삭제
+ALTER TABLE my_table23
+    DROP COLUMN city;
+
+# Column 이름 변경
+ALTER TABLE my_table23
+    RENAME COLUMN address TO town;
+
+ALTER TABLE my_table23
+    RENAME COLUMN birth TO birth_date;
+
+# Column 자료형 변경
+ALTER TABLE my_table23
+    MODIFY COLUMN price DEC(20, 2);
+
+ALTER TABLE my_table23
+    MODIFY COLUMN name VARCHAR(30);
+
+DESC my_table23;
+
+# Column 제약사항 변경
+ALTER TABLE my_table23
+    MODIFY COLUMN name VARCHAR(20) UNIQUE ;
+
+ALTER TABLE my_table23
+    MODIFY COLUMN age int(11) PRIMARY KEY ;
+
+# UNIQUE 제약사항 삭제하기
+SHOW index FROM my_table23;
+ALTER TABLE my_table23
+    DROP INDEX name;
+
+#NOT NULL 추가 및 삭제
+ALTER TABLE my_table23
+    MODIFY COLUMN name VARCHAR(20) NOT NULL ;
+
+ALTER TABLE my_table23
+    MODIFY COLUMN name VARCHAR(20) NULL ;
+
+# DEFAULT 제약사항 추가 및 삭제
+ALTER TABLE my_table23
+    MODIFY COLUMN name VARCHAR(20) DEFAULT 'guest';
+
+ALTER TABLE my_table23
+    ALTER COLUMN name DROP DEFAULT;
+
+ALTER TABLE my_table23
+    MODIFY COLUMN age INT(11) DEFAULT '20';
+
+ALTER TABLE my_table23
+    ALTER COLUMN age DROP DEFAULT ;
+
+# PRIMARY KEY 제약사항 추가 및 삭제
+ALTER TABLE my_table23
+    ADD PRIMARY KEY (name);
+
+ALTER TABLE my_table23
+    DROP PRIMARY KEY;
+ALTER TABLE my_table23
+    MODIFY COLUMN name VARCHAR(20) NULL;
