112797dc6f1ba82fae2de781e02ee54e09808db9
kooreung <crisishyun@gmail.com>
2024-04-25 16:05:47 +0900

    240425_Database_Normalization_Second Normal Form

---
+++ sql/27secondNormalForm.sql
@@ -0,0 +1,7 @@
+
+# Second Normal Form (2NF , 제 2 정규화)
+    # 조건
+    # 1) 1 정규화가 만족되어야 한다.
+    # 2) Key 가 아닌 Column 이 Key Column 일부분에 의해 결정되면 안된다.
+
+# INT PRIMARY KEY AUTO_INCREMENT Column 있으면 해결 된다.
