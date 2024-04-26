8af2e8b7e52b18da677523168ded72de6fc20515
kooreung <crisishyun@gmail.com>
2024-04-26 15:25:55 +0900

    240426_Database_Outer Join

--- sql/37outerJoin.sql
+++ sql/37outerJoin.sql
@@ -8,29 +8,35 @@ FROM category;
 
 # 상품이 존재하지 않는 카테고리 확인
 SELECT *
-FROM category c LEFT JOIN product p
-ON c.id = p.category_id
+FROM category c
+         LEFT JOIN product p
+                   ON c.id = p.category_id
 WHERE p.id IS NULL;
 
 USE w3schools;
 
 SELECT c.CustomerName
-FROM Orders o RIGHT JOIN Customers c
-ON o.CustomerID = c.CustomerID
+FROM Orders o
+         RIGHT JOIN Customers c
+                    ON o.CustomerID = c.CustomerID
 WHERE o.OrderID IS NULL;
 
 # 1996 년에 주문한 적 있는 고객들의 이름
 SELECT DISTINCT c.CustomerName
-FROM Orders o JOIN Customers c
-ON o.CustomerID = c.CustomerID
+FROM Orders o
+         JOIN Customers c
+              ON o.CustomerID = c.CustomerID
 WHERE o.OrderDate LIKE '1996-%'
 ORDER BY c.CustomerName;
 
-DELETE FROM Orders WHERE OrderDate >= '1997-01-01';
+DELETE
+FROM Orders
+WHERE OrderDate >= '1997-01-01';
 
 SELECT DISTINCT c.CustomerName
-FROM Orders o RIGHT JOIN Customers c
-ON o.CustomerID = c.CustomerID
+FROM Orders o
+         RIGHT JOIN Customers c
+                    ON o.CustomerID = c.CustomerID
 WHERE o.OrderID IS NULL;
 
 INSERT INTO Employees
@@ -39,6 +45,7 @@ VALUES ('흥민', '손'),
        ('강인', '이');
 
 SELECT e.FirstName, e.LastName
-FROM Orders o RIGHT JOIN Employees e
-ON o.EmployeeID = e.EmployeeID
+FROM Orders o
+         RIGHT JOIN Employees e
+                    ON o.EmployeeID = e.EmployeeID
 WHERE o.OrderDate IS NULL;
