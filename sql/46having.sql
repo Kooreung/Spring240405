b851b2807136a20fa44addb06de7d6b1bf257162
kooreung <crisishyun@gmail.com>
2024-04-29 15:22:35 +0900

    240429_Having

--- sql/46having.sql
+++ sql/46having.sql
@@ -15,8 +15,24 @@ GROUP BY CategoryID
 HAVING 상품수 >= 10;
 
 # 고객이 5명 이상 있는 국가들 조회
-SELECT Country, COUNT(Country) 고객수
+SELECT Country, COUNT(CustomerID) 고객수
 FROM Customers
 GROUP BY Country
 HAVING 고객수 >= 5
 ORDER BY 고객수 DESC;
+
+# 97년 7월 매출액 SUM(가격*개수) 이 1만 달러 이상인 직원들 조회
+SELECT Employees.EmployeeID,
+       LastName,
+       SUM(Price * Quantity) 판매수
+FROM Employees
+         JOIN Orders
+              ON Employees.EmployeeID = Orders.EmployeeID
+         JOIN OrderDetails
+              ON Orders.OrderID = OrderDetails.OrderID
+         JOIN Products
+              ON OrderDetails.ProductID = Products.ProductID
+WHERE OrderDate BETWEEN '1997-07-01' AND '1997-07-31'
+GROUP BY Employees.EmployeeID
+HAVING 판매수 >= 10000
+ORDER BY 판매수 DESC;
