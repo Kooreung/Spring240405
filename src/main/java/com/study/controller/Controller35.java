57aaaab2c8de966a214f8e1454ea0ba7630e0a83
kooreung <crisishyun@gmail.com>
2024-04-30 11:07:01 +0900

    240430_Transaction

--- src/main/java/com/study/controller/Controller35.java
+++ src/main/java/com/study/controller/Controller35.java
@@ -1,11 +1,13 @@
 package com.study.controller;
 
 import com.study.service.Service2;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequiredArgsConstructor
 @RequestMapping("main35")
 public class Controller35 {
     private final Service2 service;
