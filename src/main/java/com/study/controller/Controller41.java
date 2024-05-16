a18ebfc281378e444c051422de107e6dfa93be79
kooreung <crisishyun@gmail.com>
2024-05-16 12:17:39 +0900

    JSON 데이터 타입의 종류

--- src/main/java/com/study/controller/Controller41.java
+++ src/main/java/com/study/controller/Controller41.java
@@ -17,6 +17,7 @@ public class Controller41 {
     // RequestBody 로 받으면 된다.
     // Key: Value 로 구성되어 있으므로 Map 으로 받으면 된다.
     // String 과 Object
+    // 6가지 데이터 타입 중 어떤 게 들어올 지 모르니 Object 를 쓰는 것
     @PostMapping("/sub1")
     public void sub1(@RequestBody Map<String, Object> map) {
         System.out.println("Controller41.sub1");
@@ -26,6 +27,7 @@ public class Controller41 {
     @PostMapping("/sub2")
     public void sub2(@RequestBody Map<String, Object> map) {
         System.out.println("map = " + map);
+
     }
 
     @PostMapping("/sub3")
@@ -36,5 +38,21 @@ public class Controller41 {
     @PostMapping("/sub4")
     public void sub4(@RequestBody Map<String, Object> map) {
         System.out.println("map = " + map);
+    }
+
+    @PostMapping("/sub5")
+    public void sub5(@RequestBody Map<String, Object> map) {
+        System.out.println("map = " + map);
+
+        Object name = map.get("name");
+        System.out.println("name.getClass() = " + name.getClass());
+        Object age = map.get("age");
+        System.out.println("age.getClass() = " + age.getClass());
+        Object item = map.get("item");
+        System.out.println("item.getClass() = " + item.getClass());
+        Object married = map.get("married");
+        System.out.println("married.getClass() = " + married.getClass());
+        Object team = map.get("team");
+        System.out.println("team.getClass() = " + team.getClass());
     }
 }
