c367cb8132d9b7d747247b82628e57a2d8ba4c5e
kooreung <crisishyun@gmail.com>
2024-05-16 10:39:22 +0900

    get, post 요청 handler method 작성

--- src/main/java/com/study/controller/Controller40.java
+++ src/main/java/com/study/controller/Controller40.java
@@ -5,6 +5,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.time.LocalDate;
+import java.util.Arrays;
+
 @Controller
 public class Controller40 {
 
@@ -35,5 +38,32 @@ public class Controller40 {
         System.out.println("id = " + id);
         System.out.println("name = " + name);
         System.out.println("email = " + email);
+    }
+
+    @PostMapping("/api/someurl3")
+    public void method6(@RequestParam("id") Integer id,
+                        @RequestParam("city") String city,
+                        @RequestParam("country") String country) {
+        System.out.println("id = " + id);
+        System.out.println("city = " + city);
+        System.out.println("country = " + country);
+    }
+
+    @GetMapping("/api/someurl4")
+    public void method7(@RequestParam("id") Integer id,
+                        @RequestParam("name") String name,
+                        @RequestParam("email") String email,
+                        @RequestParam("birthDate") LocalDate birthDate) {
+        System.out.println("id = " + id);
+        System.out.println("name = " + name);
+        System.out.println("email = " + email);
+        System.out.println("birthDate = " + birthDate);
+    }
+
+    @PostMapping("/api/someurl4")
+    public void method8(@RequestParam("foods") String[] foods,
+                        @RequestParam("item") String item) {
+        System.out.println(Arrays.toString((foods)));
+        System.out.println("item = " + item);
     }
 }
