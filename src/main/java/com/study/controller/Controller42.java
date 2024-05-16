d2f6d3319e7bdfa57cd450b2bd80b060a9b2ba96
kooreung <crisishyun@gmail.com>
2024-05-16 15:07:03 +0900

    JavaBean 객체를 응답

--- src/main/java/com/study/controller/Controller42.java
+++ src/main/java/com/study/controller/Controller42.java
@@ -1,6 +1,7 @@
 package com.study.controller;
 
 import com.study.domain.MyBean411;
+import com.study.domain.MyBean412;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -63,5 +64,17 @@ public class Controller42 {
         obj.setAge(35);
         return obj;
         // json 으로 직렬화 된다.
+    }
+
+    @GetMapping("sub6")
+    @ResponseBody
+    public MyBean412 method6() {
+        MyBean412 myBean412 = new MyBean412();
+        myBean412.setFoods(List.of("감자", "고구마"));
+        myBean412.setName("흥민");
+        myBean412.setMarried(false);
+        MyBean411 myBean411 = new MyBean411();
+        myBean412.setInfo(myBean411);
+        return myBean412;
     }
 }
--- src/main/java/com/study/domain/MyBean412.java
+++ src/main/java/com/study/domain/MyBean412.java
@@ -2,14 +2,13 @@ package com.study.domain;
 
 import lombok.Data;
 
-import java.util.ArrayList;
-import java.util.Map;
+import java.util.List;
 
 @Data
 public class MyBean412 {
     private String name;
     private int age;
     private Boolean married;
-    private Map<String, Object> info;
-    private ArrayList<String> foods;
+    private MyBean411 info;
+    private List<String> foods;
 }
