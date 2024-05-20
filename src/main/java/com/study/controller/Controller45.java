14a486fed463d27cf054bb1b3941203eb095a4f3
kooreung <crisishyun@gmail.com>
2024-05-20 15:33:16 +0900

    MultipartFile 타입으로 파일 Request Parameter 받기 + 파일 저장

--- src/main/java/com/study/controller/Controller45.java
+++ src/main/java/com/study/controller/Controller45.java
@@ -7,16 +7,24 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+import java.io.IOException;
+
 @Controller
 @RequestMapping("/api/main45")
 public class Controller45 {
     @PostMapping("sub1")
     @ResponseBody
     public String sub1(@RequestParam("name") String name,
-                       @RequestParam("file") MultipartFile file) {
+                       @RequestParam("file") MultipartFile file) throws IOException {
         System.out.println("name = " + name);
         System.out.println("file.getOriginalFilename() = " + file.getOriginalFilename());
         System.out.println("file.getSize() = " + file.getSize());
+
+        // 파일의 저장 경로
+        String path = "C:/Temp/" + file.getOriginalFilename();
+        file.transferTo(new File(path));
+
         return null;
     }
 }
